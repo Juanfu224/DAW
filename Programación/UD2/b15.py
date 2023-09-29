6dff8630bee8adec8336ff3e02df0680c49b10c0
Juanfu224 <juanfu224@gmail.com>
2023-09-29 14:03:21 +0200

    a

--- Programación/UD2/b15.py
+++ Programación/UD2/b15.py
@@ -2,3 +2,32 @@
 Cuando se realiza la venta del producto, ésta es de un solo tipo y tamaño, se requiere determinar cuánto recibirá un productor por la uva que entrega en un embarque,
 considerando lo siguiente: si es de tipo A, se le cargan 20 céntimos al precio inicial cuando es de tamaño 1; y 30 céntimos si es de tamaño 2.
 Si es de tipo B, se rebajan 30 céntimos cuando es de tamaño 1, y 50 céntimos cuando es de tamaño 2. Realice un algoritmo para determinar la ganancia obtenida."""
+
+
+precio_kilo_uva = float(input("¿A cuanto está el kilo de uva?: "))
+tipo_uva = input("¿Tipo de uva (A/B)?: ")
+tamanio_uva = int(input("¿Tamaño de uva (1/2)?: "))
+
+precio_A1 = precio_kilo_uva+0.20
+precio_A2 = precio_kilo_uva+0.30
+precio_B1 = precio_kilo_uva-0.30
+precio_B2 = precio_kilo_uva-0.50
+
+if tipo_uva == 'A':
+    if tamanio_uva == 1:
+        precio_final = precio_A1
+    elif tamanio_uva == 2:
+        precio_final = precio_A2
+    else:
+        print("Tamaño de uva no válido")
+elif tipo_uva == 'B':
+    if tamanio_uva == 1:
+        precio_final = precio_B1
+    elif tamanio_uva == 2:
+        precio_final = precio_B2
+    else:
+        print("Tamaño de uva no válido")
+else:
+    print("Tipo de uva no válido")
+
+print("El precio final por kilo de uva es:",precio_final,"euros.")
---
+++ Programación/UD2/b17.py
@@ -0,0 +1,35 @@
+"""La política de cobro de una compañía telefónica es: cuando se realiza una llamada, el cobro es por el tiempo que ésta dura,
+de tal forma que los primeros cinco minutos cuestan 1 euro, los siguientes tres, 80 céntimos, los siguientes dos minutos, 70 céntimos,
+y a partir del décimo minuto, 50 céntimos. Además, se carga un impuesto de 3 % cuando es domingo, y si es otro día, en turno de mañana,
+15 %, y en turno de tarde, 10 %. Realice un algoritmo para determinar cuánto debe pagar por cada concepto una persona que realiza una llamada."""
+
+minutos_duracion = int(input("¿Cuanto ha durado la llamada?:"))
+dia_semana = input("Introduce el dia de la semana en el que se realizó la llamada:")
+turno = input("Introduce el turno en el que se realizó la llamada:")
+coste_llamada = 0
+impuesto = 0
+costel_total = 0
+
+if minutos_duracion <= 5:
+        coste_llamada = minutos_duracion * 1.0
+elif minutos_duracion <= 8:
+    coste_llamada = 5 * 1.0 + (minutos_duracion - 5)* 0.8
+elif minutos_duracion <= 10: 12
+coste_llamada = 5 * 1.0 + 3 * 0.8+ (minutos_duracion 8)* 0.7
+else:
+    coste_llamada = 5 * 1.0 + 3 * 0.8+ 2 * 0.7+ (minutos_duracion 10) * 0.5
+
+if dia semana == "domingo":
+    impuesto = coste llamada * 0.03
+
+elif turno == "mañana":
+    impuesto = coste_llamada * 0.15
+
+elif turno == "tarde":
+    impuesto coste llamada 0.10
+
+coste_total = coste llamada + impuesto
+
+
+
+print("El coste total de la llamada es de", round (coste_total, 2), "Euros.")
